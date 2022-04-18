3b2f59a570e090290db985dafcaa958635fd4328
TatsukiSakai <tatsukisakai@TatsukiSakainoMacBook-Pro.local>
2022-04-18 10:29:28 +0900

    CRDテンプレートファイルの作成

---
+++ api/v1/cueservicemodels_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CueServiceModelsSpec defines the desired state of CueServiceModels
+type CueServiceModelsSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of CueServiceModels. Edit cueservicemodels_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// CueServiceModelsStatus defines the observed state of CueServiceModels
+type CueServiceModelsStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// CueServiceModels is the Schema for the cueservicemodels API
+type CueServiceModels struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CueServiceModelsSpec   `json:"spec,omitempty"`
+	Status CueServiceModelsStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// CueServiceModelsList contains a list of CueServiceModels
+type CueServiceModelsList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CueServiceModels `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CueServiceModels{}, &CueServiceModelsList{})
+}
